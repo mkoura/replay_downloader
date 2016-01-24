1838aa81d6a59816b3d0b9fd38c639950e6160e8
Martin Kourim <martin@kourim.net>
2016-01-24 20:41:10 +0100

    unittest for 'parse_downloads_list'

--- tests/test_replay_downloader.py
+++ tests/test_replay_downloader.py
@@ -4,6 +4,14 @@ import replay_downloader as rd
 
 
 class TestDownloads(unittest.TestCase):
+    def test_parse_downloads_list(self):
+        l = [' # foo', 'bar', 'http://baz', ' foo1', 'http://bar1 ']
+        self.assertEqual(rd.Downloads.parse_downloads_list(l),
+                         [rd.Fileinfo(path='bar', type=0),
+                          rd.Fileinfo(path='http://baz', type=1),
+                          rd.Fileinfo(path='foo1', type=0),
+                          rd.Fileinfo(path='http://bar1', type=1)])
+
     def test_set_destdir(self):
         msg = rd.Msgs()
         conf = rd.Config('test.ini')
